98d9f00005be9f37d7486c22917d074327c08adc
Jirrod Glynn <Jirrod Glynn>
2017-05-29 15:40:14 +0100

    Changed db connect creds to static vars

--- x/queries.php
+++ x/queries.php
@@ -32,7 +32,7 @@ switch ($go) {
 }
 
 // OPEN DB CONNECTION
-$conn = mysqli_connect('localhost', 'root', 'root', 'terrapp');
+$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 if (!$conn):
     die("Connection failed: " . mysqli_connect_error());
 endif;
